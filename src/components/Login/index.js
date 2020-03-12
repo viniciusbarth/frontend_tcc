a74dbae598160c51fadac808c82b8a1ec405f1f9
viniciusbarth <viniciusbarthhh@hotmail.com>
2020-03-11 22:36:13 -0300

    Ajustes de css e ajustes no html do componente de Login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -3,35 +3,22 @@ import React from 'react';
 import "./login.css";
 
 const Login = () => (
-    <div class="container boxLogin">
-    <div class="row">
-      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-        <div class="card card-signin my-5">
-          <div class="card-body">
-            <h5 class="card-title textLogin">Login</h5>
-            <form class="form-signin">
-              <div class="form-label-group">
-                <label for="inputEmail">Email address</label>
-                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus></input>
-              </div>
-
-              <div class="form-label-group">
-                <label for="inputPassword">Password</label>
-                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required></input>
-              </div>
-
-              <div class="custom-control custom-checkbox mb-3">
-                <label class="custom-control-label" for="customCheck1">Remember password</label>
-                <input type="checkbox" class="custom-control-input" id="customCheck1"></input>
-              </div>
-
-              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Entrar</button>
-              </form>
+    <div class="login">
+        <h1 class="card-title textLogin">Login</h1>
+        <form class="form-signin">
+          <div class="form-label-group padd-10">
+            <input type="text" id="inputUser" class="form-control" placeholder="Nome de usuario" required autofocus></input>
           </div>
-        </div>
-      </div>
+
+          <div class="form-label-group padd-10">
+            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required></input>
+          </div>
+
+          <input type="submit" name="" value="Login"></input>
+          <a href="#">Esqueceu a sua senha?</a><br></br>
+          <a href="#">Ainda não possui uma conta?</a>
+        </form>
     </div>
-  </div>
 );
 
 export default Login;
