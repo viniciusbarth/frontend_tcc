a912785d4c1a67406590e16b03a5957eddfee76c
vinicius gabriel barth <viniciusbarthhh@hotmail.com>
2020-03-24 20:25:46 -0300

    melhorado a forma como está sendo feita o agroMenu

--- src/components/agroMenu/index.js
+++ src/components/agroMenu/index.js
@@ -5,27 +5,38 @@ import "./agroMenu.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 
-const AgroMenu = () => (
-<React.Fragment>
+class AgroMenu extends React.Component{
 
-  <div class="sidenav">
-    <a href="#home">Home</a>
-    <a href="#cadastros">Cadastros</a>
-    <a href="#AnalisePreditiva">Análise Preditiva</a>
-    <a href="#contato">Contato</a>
+  onsubmit = () => {
+    
+    alert('testando 1 2 3')
 
-    <p>Versão 1.0.0.0</p>
-    <p>Sistema AgroSoftware</p>
-  </div>
+  };
 
-  <div class="main">
-    <div class="topnav">
-      <span href="" className="iconLogout">
-      <FontAwesomeIcon icon={faSignOutAlt}></FontAwesomeIcon>
-      </span>
-    </div>
-  </div>
-</React.Fragment>
-);
+  render(){
+    return(
+      <React.Fragment>
+
+        <div class="sidenav">
+          <a onClick={this.home}>Home</a>
+          <a onClick={this.onsubmit}>Cadastros</a>
+          <a href="#AnalisePreditiva">Análise Preditiva</a>
+          <a href="#contato">Contato</a>
+
+          <p>Versão 1.0.0.0</p>
+          <p>Sistema AgroSoftware</p>
+        </div>
+
+        <div class="main">
+          <div class="topnav">
+            <span href="" className="iconLogout">
+            <FontAwesomeIcon icon={faSignOutAlt}></FontAwesomeIcon>
+            </span>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 
 export default AgroMenu;
