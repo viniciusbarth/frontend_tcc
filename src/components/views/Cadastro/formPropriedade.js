b7182e614168eeed4d7622ad5a2d759bcc80adcc
vinicius gabriel barth <viniciusbarthhh@hotmail.com>
2020-04-09 21:36:55 -0300

    -Arrumando erro no form de propriedade

--- src/components/views/Cadastro/formPropriedade.js
+++ src/components/views/Cadastro/formPropriedade.js
@@ -26,11 +26,11 @@ export default class Login extends React.Component {
 								<legend>Cadastro de Propriedade</legend>
 								<div className="form-group">
 									<label htmlFor="nomePropriedade">Nome Propriedade</label>
-									<input type="text" className="form-control" id="nomePropriedade" name="nomePropriedade" onChange={this.onChange} value={this.state.email} placeholder="Ex:. Fazenda Boa Fé" />
+									<input type="text" className="form-control" id="nomePropriedade" name="nomePropriedade"  placeholder="Ex:. Fazenda Boa Fé" />
 								</div>
 								<div className="form-group">
 									<label htmlFor="descPropriedade">Descrição da Propriedade</label>
-									<input type="text" className="form-control" id="descPropriedade" name="descPropriedade" placeholder="Descrição geral da propriedade" />
+									<input type="text" className="form-control" id="descPropriedade" name="descPropriedade"  placeholder="Descrição geral da propriedade" />
 								</div>
 								<button type="button" onClick={this.onSubmit} className="btn btn-success">Salvar</button>
 							</fieldset>
