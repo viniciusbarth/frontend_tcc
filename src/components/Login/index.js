5c65325bc08d439daf7c144e39df82648176a993
viniciusbarth <viniciusbarthhh@hotmail.com>
2020-05-31 15:20:41 -0300

    Ajuste de css

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -54,11 +54,11 @@ class Login extends React.Component{
             </div>
             <form className="form-signin">
               <div className="form-label-group padd-10">
-                <input type="email" id="inputUser" name="usuDsEmail"  onChange={this.onChange} value={this.state.usuDsEmail} className="form-control" placeholder="Nome de usuário" required></input>
+                <input type="email" id="inputUser" name="usuDsEmail"  onChange={this.onChange} value={this.state.usuDsEmail} className="form-control inputLogin" placeholder="Email do usuário" required></input>
               </div>
 
               <div className="form-label-group padd-10">
-                <input type="password" id="inputPassword" name="usuDsSenha" onChange={this.onChange} value={this.state.usuDsSenha} className="form-control" placeholder="Senha" required></input>
+                <input type="password" id="inputPassword" name="usuDsSenha" onChange={this.onChange} value={this.state.usuDsSenha} className="form-control inputSenha" placeholder="Senha" required></input>
               </div>
 
               <button className="buttonLogin" type="btn btn-primary" onClick={this.onSubmit} name="" value="Login">Login</button>
